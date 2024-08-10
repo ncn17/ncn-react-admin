fb30b400c3fcdefa50f2aa505f75c4b7e82d25f6
ncn17 <nkounkounicephore@gmail.com>
2024-08-10 22:42:40 +0200

    Add calendar page with events

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,4 +1,13 @@
-import { Box, useTheme } from '@mui/material';
+/* eslint-disable no-underscore-dangle */
+import { useState } from 'react';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -6,7 +15,6 @@ import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
 import { Header } from '../../components/header';
 import { tokens } from '../../theme';
-import { useState } from 'react';
 
 export const Calendar = () => {
   const theme = useTheme();
@@ -29,8 +37,9 @@ export const Calendar = () => {
     }
   };
 
-  const handleEventClick = (selected) => {
+  const handleEventClick = (selected: any) => {
     if (
+      // eslint-disable-next-line no-alert
       window.confirm(
         `Are you sure you want to delete the event '${selected.event.title}'`
       )
@@ -39,36 +48,85 @@ export const Calendar = () => {
     }
   };
 
+  const parseDate = (value: any) => {
+    const rawDate = Date.parse(value);
+
+    return new Date(rawDate).toLocaleDateString('fr-FR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  };
+
   return (
     <Box m={2}>
       <Header title="Calendar" subTitle="Full Calendar Interactive Page" />
-      <Box flex="1 1 100%" ml="15px">
-        <FullCalendar
-          height="75vh"
-          plugins={[
-            dayGridPlugin,
-            timeGridPlugin,
-            interactionPlugin,
-            listPlugin,
-          ]}
-          initialView="dayGridMonth"
-          editable
-          selectable
-          selectMirror
-          dayMaxEvents
-          initialEvents={[
-            {
-              id: '12315',
-              title: 'All-day event',
-              date: '2024-08-14',
-            },
-            {
-              id: '5123',
-              title: 'Timed event',
-              date: '2025-08-28',
-            },
-          ]}
-        />
+      <Box display="flex" justifyContent="space-between">
+        <Box
+          mt={2}
+          flex="1 1 20%"
+          sx={{
+            bgcolor: colors.primary[400],
+            padding: '15px',
+            borderRadius: '4px',
+          }}
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event: any) => (
+              <ListItem
+                key={event._def.publicId}
+                sx={{
+                  backgroundColor: colors.blueAccent[600],
+                  margin: '10px 0',
+                  borderRadius: '2px',
+                }}
+              >
+                <ListItemText
+                  primary={event._def.title}
+                  secondary={
+                    <Typography>
+                      {parseDate(event._instance.range.start)}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            initialView="dayGridMonth"
+            editable
+            selectable
+            selectMirror
+            dayMaxEvents
+            weekends={false}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+                id: '12315',
+                title: 'All-day event',
+                date: '2024-08-14',
+              },
+              {
+                id: '5123',
+                title: 'Timed event',
+                date: '2025-08-28',
+              },
+            ]}
+          />
+        </Box>
       </Box>
     </Box>
   );
--- src/pages/profiles/index.tsx
+++ src/pages/profiles/index.tsx
@@ -42,59 +42,61 @@ export const Profiles = () => {
     <Box m={2}>
       <Header title="create user" subTitle="Create a New User Profile" />
 
-      <AppForm
-        initialValues={initialValues}
-        validationSchema={controlSchema}
-        onSubmit={handleFormSubmit}
-      >
-        <Box>
-          <Box
-            display="grid"
-            gap="30px"
-            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-            sx={{
-              '& > div': { gridColumn: isNoMobile ? undefined : 'span 4' },
-            }}
-          >
-            <TextFieldForm
-              name="firstName"
-              label="First Name"
-              sx={{ gridColumn: 'span 2' }}
-            />
-            <TextFieldForm
-              name="lastName"
-              label="Last Name"
-              sx={{ gridColumn: 'span 2' }}
-            />
-            <TextFieldForm
-              name="email"
-              label="Email"
-              type="email"
-              sx={{ gridColumn: 'span 4' }}
-            />
-            <TextFieldForm
-              name="contact"
-              label="Contact Number"
-              sx={{ gridColumn: 'span 4' }}
-            />
-            <TextFieldForm
-              name="address1"
-              label="Address 1"
-              sx={{ gridColumn: 'span 4' }}
-            />
-            <TextFieldForm
-              name="address2"
-              label="Address é"
-              sx={{ gridColumn: 'span 4' }}
-            />
+      <Box mt={2}>
+        <AppForm
+          initialValues={initialValues}
+          validationSchema={controlSchema}
+          onSubmit={handleFormSubmit}
+        >
+          <Box>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+              sx={{
+                '& > div': { gridColumn: isNoMobile ? undefined : 'span 4' },
+              }}
+            >
+              <TextFieldForm
+                name="firstName"
+                label="First Name"
+                sx={{ gridColumn: 'span 2' }}
+              />
+              <TextFieldForm
+                name="lastName"
+                label="Last Name"
+                sx={{ gridColumn: 'span 2' }}
+              />
+              <TextFieldForm
+                name="email"
+                label="Email"
+                type="email"
+                sx={{ gridColumn: 'span 4' }}
+              />
+              <TextFieldForm
+                name="contact"
+                label="Contact Number"
+                sx={{ gridColumn: 'span 4' }}
+              />
+              <TextFieldForm
+                name="address1"
+                label="Address 1"
+                sx={{ gridColumn: 'span 4' }}
+              />
+              <TextFieldForm
+                name="address2"
+                label="Address é"
+                sx={{ gridColumn: 'span 4' }}
+              />
+            </Box>
+            <Box display="flex" mt={4}>
+              <Button type="submit" color="secondary" variant="contained">
+                Create new User
+              </Button>
+            </Box>
           </Box>
-          <Box display="flex" mt={4}>
-            <Button type="submit" color="secondary" variant="contained">
-              Create new User
-            </Button>
-          </Box>
-        </Box>
-      </AppForm>
+        </AppForm>
+      </Box>
     </Box>
   );
 };
